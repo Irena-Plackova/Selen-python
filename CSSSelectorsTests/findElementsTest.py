89e7526dfa319fcf6e4e623d9494dcd8be33c7d3
iris <irisik1316@outlook.com>
2024-09-05 22:58:09 +0200

    first commit

---
+++ CSSSelectorsTests/findElementsTest.py
@@ -0,0 +1,23 @@
+import time
+from typing import final
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+
+driver.find_element(By.ID, "autosuggest").send_keys("ind")
+time.sleep(2)
+
+countries = driver.find_elements(By.CSS_SELECTOR,"li[class='ui-menu-item'] a")
+print(len(countries))
+
+for country in countries:
+    if country.text == "India":
+        country.click()
+        break
+
+assert driver.find_element(By.ID, "autosuggest").get_attribute("value") == "India"
+
+time.sleep(6)
